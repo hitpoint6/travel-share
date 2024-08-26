ddca72177c9ae677f6df60ae18786d81aab7b36f
hitpoint6 <hitpoint6@gmail.com>
2024-08-26 11:22:15 -0700

    Add search position in map

--- react-vite/src/components/ActivitiesForm/ActivitiesForm.jsx
+++ react-vite/src/components/ActivitiesForm/ActivitiesForm.jsx
@@ -44,152 +44,211 @@ function ActivitiesForm() {
         setActivityData({ ...activityData, [e.target.name]: e.target.value });
     };
 
+    const setPosition = (searchPosition) => {
+      setActivityData({
+        ...activityData,
+        longitude: searchPosition.longitude,
+        latitude: searchPosition.latitude,
+        place: searchPosition.label.split(",")[0],
+      });
+    };
+
     const resetForm = () => {
-        setActivityData({
-            place: "",
-            longitude: "",
-            latitude: "",
-            description: "",
-            place_image_url: "",
-            schedule_id: null,
-        });
-        setShowActivityForm(false);
-        setIsEditing(false);
-        setCurrentActivityId(null);
+      setActivityData({
+        place: "",
+        longitude: "",
+        latitude: "",
+        description: "",
+        place_image_url: "",
+        schedule_id: null,
+      });
+      setShowActivityForm(false);
+      setIsEditing(false);
+      setCurrentActivityId(null);
     };
 
     const handleEditActivity = (activity) => {
-        setActivityData({
-            place: activity.place,
-            longitude: activity.longitude,
-            latitude: activity.latitude,
-            description: activity.description,
-            place_image_url: activity.place_image_url,
-            schedule_id: activity.schedule_id,
-        });
-        setCurrentActivityId(activity.id);
-        setIsEditing(true);
-        setShowActivityForm(true);
+      setActivityData({
+        place: activity.place,
+        longitude: activity.longitude,
+        latitude: activity.latitude,
+        description: activity.description,
+        place_image_url: activity.place_image_url,
+        schedule_id: activity.schedule_id,
+      });
+      setCurrentActivityId(activity.id);
+      setIsEditing(true);
+      setShowActivityForm(true);
     };
 
     const handleRemoveActivity = async (activityId) => {
-        console.log(activityId);
-        await dispatch(thunkDeleteActivity(activityId));
-        await dispatch(thunkItineraryById(itineraryId));
+      console.log(activityId);
+      await dispatch(thunkDeleteActivity(activityId));
+      await dispatch(thunkItineraryById(itineraryId));
     };
 
     const handleFormSubmit = async (e) => {
-        e.preventDefault();
-    
-        if (isEditing && currentActivityId) {
-            const updatedActivity = await dispatch(thunkUpdateActivity({...activityData, id: currentActivityId}));
-            if (updatedActivity) {
-                await dispatch(thunkItineraryById(itineraryId));
-                resetForm();
-            }
-        } else {
-            const newActivity = await dispatch(thunkNewActivity(activityData));
-            if (newActivity) {
-                await dispatch(thunkItineraryById(itineraryId));
-                resetForm();
-            }
+      e.preventDefault();
+
+      if (isEditing && currentActivityId) {
+        const updatedActivity = await dispatch(
+          thunkUpdateActivity({ ...activityData, id: currentActivityId })
+        );
+        if (updatedActivity) {
+          await dispatch(thunkItineraryById(itineraryId));
+          resetForm();
         }
+      } else {
+        const newActivity = await dispatch(thunkNewActivity(activityData));
+        if (newActivity) {
+          await dispatch(thunkItineraryById(itineraryId));
+          resetForm();
+        }
+      }
     };
 
     if (!itinerary) return <Loading />;
 
     return (
-    <main className="itinerary-detail-page activity-edit-page">
+      <main className="itinerary-detail-page activity-edit-page">
         <div className="itinerary-content">
-            <div className="header">
-                <img src={itinerary.preview_image_url} alt={itinerary.title} />
-                <h1>{itinerary.title}</h1>
-            </div>
-            <div className="body">
+          <div className="header">
+            <img src={itinerary.preview_image_url} alt={itinerary.title} />
+            <h1>{itinerary.title}</h1>
+          </div>
+          <div className="body">
             <div className="inline">
-                <div className="user-profile">
-                    <img className="profile-image" src={itinerary.traveler.profile_url} alt={itinerary.traveler_id} /><span>{itinerary.traveler.username}</span>
-                </div>
-                <div className="detail-page-button"><button onClick={() => navigate(`/itineraries/${itinerary.id}/edit`)}>
-                Edit itinerary</button></div>
+              <div className="user-profile">
+                <img
+                  className="profile-image"
+                  src={itinerary.traveler.profile_url}
+                  alt={itinerary.traveler_id}
+                />
+                <span>{itinerary.traveler.username}</span>
+              </div>
+              <div className="detail-page-button">
+                <button
+                  onClick={() => navigate(`/itineraries/${itinerary.id}/edit`)}
+                >
+                  Edit itinerary
+                </button>
+              </div>
             </div>
             <div className="description">
-                <p>{itinerary.description}</p>
+              <p>{itinerary.description}</p>
             </div>
             <div className="schedules-container">
-                {schedules && schedules.map(schedule => (
-                    <div key={schedule.id} className="schedule-item">
-                        <h2>{schedule.day}</h2>
-                        {schedule.activities && schedule.activities.map(activity => (
-                            <div key={activity.id} className="activity-item">
-                                <div className="activity-info">
-                                    <span className="activity-place"><FaLocationArrow />{activity.place}</span> 
-                                    <p>{activity.description}</p>
-                                    <div className="activity-control">
-                                        <span onClick={() => handleEditActivity(activity)}><p>Edit activity</p><FaEdit /></span>
-                                        <span onClick={() => handleRemoveActivity(activity.id)}><p>Remove activity</p><FaDeleteLeft /></span>
-                                       
-                                        
-                                    </div>
-                                </div>
-                                <div className="activity-image">
-                                    {activity.place_image_url && <img src={activity.place_image_url} alt={activity.id} />}
-                                    
-                                </div>
+              {schedules &&
+                schedules.map((schedule) => (
+                  <div key={schedule.id} className="schedule-item">
+                    <h2>{schedule.day}</h2>
+                    {schedule.activities &&
+                      schedule.activities.map((activity) => (
+                        <div key={activity.id} className="activity-item">
+                          <div className="activity-info">
+                            <span className="activity-place">
+                              <FaLocationArrow />
+                              {activity.place}
+                            </span>
+                            <p>{activity.description}</p>
+                            <div className="activity-control">
+                              <span
+                                onClick={() => handleEditActivity(activity)}
+                              >
+                                <p>Edit activity</p>
+                                <FaEdit />
+                              </span>
+                              <span
+                                onClick={() =>
+                                  handleRemoveActivity(activity.id)
+                                }
+                              >
+                                <p>Remove activity</p>
+                                <FaDeleteLeft />
+                              </span>
                             </div>
-                        ))}
-                    <div onClick={() => handleAddActivityClick(schedule.id)} className="add-day-activity"><button>+ Add activity</button></div>
-                    {showActivityForm && activityData.schedule_id === schedule.id && (
-                        <form onSubmit={handleFormSubmit} className="activity-form">
-                            <input
-                                type="text"
-                                name="place"
-                                value={activityData.place}
-                                onChange={handleInputChange}
-                                placeholder="Place"
-                                required
-                            />
-                            <input
-                                type="text"
-                                name="longitude"
-                                value={activityData.longitude}
-                                onChange={handleInputChange}
-                                placeholder="Longitude"
-                                required
-                            />
-                            <input
-                                type="text"
-                                name="latitude"
-                                value={activityData.latitude}
-                                onChange={handleInputChange}
-                                placeholder="Latitude"
-                                required
-                            />
-                            <textarea
-                                name="description"
-                                value={activityData.description}
-                                onChange={handleInputChange}
-                                placeholder="Description"
-                            />
-                            <input
-                                type="text"
-                                name="place_image_url"
-                                value={activityData.place_image_url}
-                                onChange={handleInputChange}
-                                placeholder="Image URL"
-                            />
-                            <button type="submit">Save</button>
-                        </form>
-                    )}
+                          </div>
+                          <div className="activity-image">
+                            {activity.place_image_url && (
+                              <img
+                                src={activity.place_image_url}
+                                alt={activity.id}
+                              />
+                            )}
+                          </div>
+                        </div>
+                      ))}
+                    <div
+                      onClick={() => handleAddActivityClick(schedule.id)}
+                      className="add-day-activity"
+                    >
+                      <button>+ Add activity</button>
                     </div>
+                    {showActivityForm &&
+                      activityData.schedule_id === schedule.id && (
+                        <form
+                          onSubmit={handleFormSubmit}
+                          className="activity-form"
+                        >
+                          <input
+                            type="text"
+                            name="place"
+                            value={activityData.place}
+                            onChange={handleInputChange}
+                            placeholder="Place"
+                            required
+                          />
+                          <input
+                            type="text"
+                            name="longitude"
+                            value={activityData.longitude}
+                            onChange={handleInputChange}
+                            placeholder="Longitude"
+                            required
+                          />
+                          <input
+                            type="text"
+                            name="latitude"
+                            value={activityData.latitude}
+                            onChange={handleInputChange}
+                            placeholder="Latitude"
+                            required
+                          />
+                          <textarea
+                            name="description"
+                            value={activityData.description}
+                            onChange={handleInputChange}
+                            placeholder="Description"
+                          />
+                          <input
+                            type="text"
+                            name="place_image_url"
+                            value={activityData.place_image_url}
+                            onChange={handleInputChange}
+                            placeholder="Image URL"
+                          />
+                          <button type="submit">Save</button>
+                        </form>
+                      )}
+                  </div>
                 ))}
             </div>
-            </div>
-            <div className="confirm-info" onClick={() => navigate(`/itineraries/${itinerary.id}`)}><button>Submit</button></div>
+          </div>
+          <div
+            className="confirm-info"
+            onClick={() => navigate(`/itineraries/${itinerary.id}`)}
+          >
+            <button>Submit</button>
+          </div>
         </div>
-        <Map itinerary={itinerary}/>    
-    </main> 
-    )
+        <Map
+          itinerary={itinerary}
+          currentActivityId={currentActivityId}
+          o
+          onSetPosition={setPosition}
+        />
+      </main>
+    );
 }  
 
 
--- react-vite/src/components/ItineraryDetailPage/ItineraryDetailPage.jsx
+++ react-vite/src/components/ItineraryDetailPage/ItineraryDetailPage.jsx
@@ -40,61 +40,102 @@ function ItineraryDetail() {
     };
 
     return (
-    <main className="itinerary-detail-page">
+      <main className="itinerary-detail-page">
         <div className="itinerary-content">
-            <div className="header">
-                <img src={itinerary.preview_image_url} alt={itinerary.title} />
-                <h1>{itinerary.title}</h1>
-            </div>
-            <div className="body">
+          <div className="header">
+            <img src={itinerary.preview_image_url} alt={itinerary.title} />
+            <h1>{itinerary.title}</h1>
+          </div>
+          <div className="body">
             <div className="inline">
-                <Link to={`/itineraries/traveler/${itinerary.traveler.id}`}>
+              <Link to={`/itineraries/traveler/${itinerary.traveler.id}`}>
                 <div className="user-profile">
-                    <img className="profile-image" src={itinerary.traveler.profile_url} alt={itinerary.traveler_id} /><span>{itinerary.traveler.username}</span>
+                  <img
+                    className="profile-image"
+                    src={itinerary.traveler.profile_url}
+                    alt={itinerary.traveler_id}
+                  />
+                  <span>{itinerary.traveler.username}</span>
                 </div>
-                </Link>
-                {user? user?.id == itinerary.traveler?.id? (
-                    <div className="detail-page-button" onClick={() => navigate(`/itineraries/${itinerary.id}/edit`)}><button>Edit itinerary</button></div>
-                ): <div className="detail-page-button" onClick={() => handleCollectClick(itinerary.id)}><button>Add to collection</button></div>: 
+              </Link>
+              {user ? (
+                user?.id == itinerary.traveler?.id ? (
+                  <div
+                    className="detail-page-button"
+                    onClick={() =>
+                      navigate(`/itineraries/${itinerary.id}/edit`)
+                    }
+                  >
+                    <button>Edit itinerary</button>
+                  </div>
+                ) : (
+                  <div
+                    className="detail-page-button"
+                    onClick={() => handleCollectClick(itinerary.id)}
+                  >
+                    <button>Add to collection</button>
+                  </div>
+                )
+              ) : (
                 <div className="detail-page-button">
-                    <button>
+                  <button>
                     <OpenModalMenuItem
-                    itemText="Add to collection"
-                    modalComponent={<LoginFormModal text={'Before you do that... please'} />}
+                      itemText="Add to collection"
+                      modalComponent={
+                        <LoginFormModal text={"Before you do that... please"} />
+                      }
                     />
-                    </button>
-                </div>}
+                  </button>
+                </div>
+              )}
             </div>
             <div className="time">
-                <p>{itinerary.updated_at}</p>
+              <p>{itinerary.updated_at}</p>
             </div>
             <div className="description">
-                <p>{itinerary.description}</p>
+              <p>{itinerary.description}</p>
             </div>
             <div className="schedules-container">
-                {schedules && schedules.map(schedule => (
-                    <div key={schedule.id} className="schedule-item">
-                        <h2>{schedule.day}</h2>
-                        {schedule.activities && schedule.activities.map(activity => (
-                            <div key={activity.id} className="activity-item">
-                                <div className="activity-info">
-                                    <span className="activity-place"><FaLocationArrow />{activity.place}</span> 
-                                    {activity.description && <p>{activity.description}</p>}
-                                </div>
-                                {activity.place_image_url && <div className="activity-image"><img src={activity.place_image_url} alt={activity.id} /></div>}
+              {schedules &&
+                schedules.map((schedule) => (
+                  <div key={schedule.id} className="schedule-item">
+                    <h2>{schedule.day}</h2>
+                    {schedule.activities &&
+                      schedule.activities.map((activity) => (
+                        <div key={activity.id} className="activity-item">
+                          <div className="activity-info">
+                            <span className="activity-place">
+                              <FaLocationArrow />
+                              {activity.place}
+                            </span>
+                            {activity.description && (
+                              <p>{activity.description}</p>
+                            )}
+                          </div>
+                          {activity.place_image_url && (
+                            <div className="activity-image">
+                              <img
+                                src={activity.place_image_url}
+                                alt={activity.id}
+                              />
                             </div>
-                        ))}
-                    </div>
+                          )}
+                        </div>
+                      ))}
+                  </div>
                 ))}
             </div>
-            <div className="discover-more" onClick={() => navigate("/itineraries")}><button>Continue explore</button></div>
-
+            <div
+              className="discover-more"
+              onClick={() => navigate("/itineraries")}
+            >
+              <button>Continue explore</button>
+            </div>
+          </div>
         </div>
-        </div>
-        <Map itinerary={itinerary}/>
-        
-    </main> 
-    )
+        <Map itinerary={itinerary} currentItinerary={1} />
+      </main>
+    );
 }  
 
 
